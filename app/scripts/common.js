aa469c03c186c57ae3c058102d72323676e9cb02
Anton Bersenyov <flutterravie@gmail.com>
2015-05-01 15:00:58 +0500

    add melt function

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,25 +1,29 @@
 $(function () {
 
 	'use strict';
-
-	var	tHeat, tBoil, m, tempStart, dHeight, waterH, freq, stopPushed;
-
-	// temp
-	tempStart = 10;
-	m = 0.75;
-	freq = 1000;
-	stopPushed = false;
-	waterH = $('.js-water').height();
+	// 1px воды = 3мл
+	var	tHeat, // время нагревания, сек
+		tBoil, // время выкипания, сек
+		dHeight, // высота шага выкипания, px
+		m = 0.75, // масса/объём, кг/л
+		tempStart = 10, // стартовая температура
+		freq = 1000, // частота обновления, мс
+		stopPushed = false,
+		waterH = $('.js-water').height(), // высота блока воды
+		iceW = $('.js-ice').width(),
+		iceH = $('.js-ice').height();
 
 	$('.js-start').on('click', function () {
-		countT();
+		melt();
+		// countT();
 
 		/*do {
-			heat ();
+			heat();
 		} while (tHeat > 0);*/
-		
-		dHeight = m * 200 / tBoil;
-		boil ();
+
+		// dHeight = m * 200 / tBoil;
+
+		// boil();
 	});
 
 	$('.js-stop').on('click', function () {
@@ -30,7 +34,23 @@ $(function () {
 	function countT() {
 		tHeat = Math.ceil(4.187 * m * (100 - tempStart) / 2); // t = c(кДж/кг*К) * m(кг) * dT(°c) / p(кВт)
 		tBoil = Math.ceil(2256 * m / 2);
-		console.log(tBoil);
+	}
+
+	function melt() {
+		var tMelt = 100,
+			dIceWidth = 200 / tMelt,
+			dIceHeight = 1.05 * (waterH / tMelt),
+			counterID = setInterval(function () {
+				iceW = iceW - dIceWidth;
+				iceH = iceH - dIceHeight;
+				$('.js-ice').width(iceW).height(iceH);
+				tMelt--;
+				if (tMelt <= 0) {
+					clearInterval(counterID);
+				}
+				console.log(iceW, iceH, tMelt);
+			}, 25);
+		console.log('end');
 	}
 
 	function heat() {
@@ -38,7 +58,7 @@ $(function () {
 	}
 
 	function boil() {
-		var counterID = setInterval(function() {
+		var counterID = setInterval(function () {
 			waterH = waterH - dHeight;
 			$('.js-water').height(waterH);
 			tBoil--;
@@ -49,17 +69,10 @@ $(function () {
 			}
 			if (tBoil <= 0) {
 				clearInterval(counterID);
-			};
+			}
 		}, freq);
 	}
 
-	/*var navTab = $('.js-tab');
 
-	navTab.on('click', function () {
-		if (!$(this).is('.cond-ui-nav__item_active')) {
-			navTab.toggleClass('cond-ui-nav__item_active');
-			$('.js-tab-content').toggleClass('cond-ui-wrapper_active');
-		}
-	});*/
 
 });
